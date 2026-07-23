550c59aa2547b1c49d086c20c1dd226dd0ef25ea
sepehratwork <sepehratwork@gmail.com>
2026-07-23 14:00:37 +0330

    debugging evaluation

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .callbacks import GradientMetricsCallback
-from .checkpoint import get_latest_checkpoint, get_resume_state, cleanup_checkpoints, clear_all_checkpoints
+from .checkpoint import get_latest_checkpoint, get_resume_state, cleanup_checkpoints, clear_all_checkpoints, save_to_hf_hub
 from .generation import generate_completions
