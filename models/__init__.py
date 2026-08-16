aef9c536240c4827e7413aff1d250dd183ee65d9
sepehratwork <sepehratwork@gmail.com>
2026-08-16 17:34:45 +0330

    Debugging Language Models

--- models/__init__.py
+++ models/__init__.py
@@ -1,22 +1,39 @@
 from .architectures import (
+    # OLMo 3
     OLMo3TestConfig, 
     OLMo3ForCausalLM,
+    # DeepSeek V4
     DeepSeekV4Config,
+    DeepSeekV4TestConfig,
     DeepSeekV4ForCausalLM,
+    # GLM 5
     GLM5Config,
+    GLM5TestConfig,
     GLM5ForCausalLM,
+    # Kimi K3
     KimiK3Config,
+    KimiK3TestConfig,
     KimiK3ForCausalLM,
+    # Magistral
     MagistralConfig,
+    MagistralTestConfig,
     MagistralForCausalLM,
+    # MiniMax M2
     MiniMaxM2Config,
+    MiniMaxM2TestConfig,
     MiniMaxM2ForCausalLM,
+    # MobileLLM
     MobileLLMProConfig,
+    MobileLLMProTestConfig,
     MobileLLMProForCausalLM,
+    # Nemotron 3
     Nemotron3Config,
+    Nemotron3TestConfig,
     Nemotron3ForCausalLM,
     Nemotron3DenseForCausalLM,
+    # Qwen 3
     Qwen3Config,
+    Qwen3TestConfig,
     Qwen3ForCausalLM,
     Qwen3MoEForCausalLM,
 )
@@ -47,17 +64,36 @@ from .utils import (
 
 # Registry mapping model architecture names to Config and Model classes
 MODEL_REGISTRY = {
-    "olmo_3": (OLMo3TestConfig, OLMo3ForCausalLM),
+    # OLMo 3
+    "olmo_3_test": (OLMo3TestConfig, OLMo3ForCausalLM),
+    # DeepSeek V4
     "deepseek_v4": (DeepSeekV4Config, DeepSeekV4ForCausalLM),
+    "deepseek_v4_test": (DeepSeekV4TestConfig, DeepSeekV4ForCausalLM),
+    # GLM 5
     "glm_5": (GLM5Config, GLM5ForCausalLM),
+    "glm_5_test": (GLM5TestConfig, GLM5ForCausalLM),
+    # Kimi K3
     "kimi_k3": (KimiK3Config, KimiK3ForCausalLM),
+    "kimi_k3_test": (KimiK3TestConfig, KimiK3ForCausalLM),
+    # Magistral
     "magistral": (MagistralConfig, MagistralForCausalLM),
+    "magistral_test": (MagistralTestConfig, MagistralForCausalLM),
+    # MiniMax M2
     "minimax_m2": (MiniMaxM2Config, MiniMaxM2ForCausalLM),
+    "minimax_m2_test": (MiniMaxM2TestConfig, MiniMaxM2ForCausalLM),
+    # MobileLLM Pro
     "mobilellm_pro": (MobileLLMProConfig, MobileLLMProForCausalLM),
+    "mobilellm_pro_test": (MobileLLMProTestConfig, MobileLLMProForCausalLM),
+    # Nemotron 3
     "nemotron_3": (Nemotron3Config, Nemotron3ForCausalLM),
+    "nemotron_3_test": (Nemotron3TestConfig, Nemotron3ForCausalLM),
     "nemotron_3_dense": (Nemotron3Config, Nemotron3DenseForCausalLM),
+    "nemotron_3_dense_test": (Nemotron3TestConfig, Nemotron3DenseForCausalLM),
+    # Qwen 3
     "qwen_3": (Qwen3Config, Qwen3ForCausalLM),
+    "qwen_3_test": (Qwen3TestConfig, Qwen3ForCausalLM),
     "qwen_3_moe": (Qwen3Config, Qwen3MoEForCausalLM),
+    "qwen_3_moe_test": (Qwen3TestConfig, Qwen3MoEForCausalLM),
 }
 
 
--- models/architectures/__init__.py
+++ models/architectures/__init__.py
@@ -1,32 +1,49 @@
 from .olmo_3 import OLMo3TestConfig, OLMo3ForCausalLM
-from .deepseek_v4 import DeepSeekV4Config, DeepSeekV4ForCausalLM
-from .glm_5 import GLM5Config, GLM5ForCausalLM
-from .kimi_k3 import KimiK3Config, KimiK3ForCausalLM
-from .magistral import MagistralConfig, MagistralForCausalLM
-from .minimax_m2 import MiniMaxM2Config, MiniMaxM2ForCausalLM
-from .mobilellm_pro import MobileLLMProConfig, MobileLLMProForCausalLM
-from .nemotron_3 import Nemotron3Config, Nemotron3ForCausalLM, Nemotron3DenseForCausalLM
-from .qwen_3 import Qwen3Config, Qwen3ForCausalLM, Qwen3MoEForCausalLM
+from .deepseek_v4 import DeepSeekV4Config, DeepSeekV4TestConfig, DeepSeekV4ForCausalLM
+from .glm_5 import GLM5Config, GLM5TestConfig, GLM5ForCausalLM
+from .kimi_k3 import KimiK3Config, KimiK3TestConfig, KimiK3ForCausalLM
+from .magistral import MagistralConfig, MagistralTestConfig, MagistralForCausalLM
+from .minimax_m2 import MiniMaxM2Config, MiniMaxM2TestConfig, MiniMaxM2ForCausalLM
+from .mobilellm_pro import MobileLLMProConfig, MobileLLMProTestConfig, MobileLLMProForCausalLM
+from .nemotron_3 import Nemotron3Config, Nemotron3TestConfig, Nemotron3ForCausalLM, Nemotron3DenseForCausalLM
+from .qwen_3 import Qwen3Config, Qwen3TestConfig, Qwen3ForCausalLM, Qwen3MoEForCausalLM
 
 __all__ = [
+    # OLMo 3
     "OLMo3TestConfig",
     "OLMo3ForCausalLM",
-    "Qwen3Config",
-    "Qwen3ForCausalLM",
-    "Qwen3MoEForCausalLM",
+    # DeepSeek V4
     "DeepSeekV4Config",
+    "DeepSeekV4TestConfig",
     "DeepSeekV4ForCausalLM",
+    # GLM 5
     "GLM5Config",
+    "GLM5TestConfig",
     "GLM5ForCausalLM",
+    # Kimi K3
     "KimiK3Config",
+    "KimiK3TestConfig",
     "KimiK3ForCausalLM",
+    # Magistral
     "MagistralConfig",
+    "MagistralTestConfig",
     "MagistralForCausalLM",
+    # MiniMax M2
     "MiniMaxM2Config",
+    "MiniMaxM2TestConfig",
     "MiniMaxM2ForCausalLM",
+    # MobileLLM Pro
     "MobileLLMProConfig",
+    "MobileLLMProTestConfig",
     "MobileLLMProForCausalLM",
+    # Nemotron 3
     "Nemotron3Config",
+    "Nemotron3TestConfig",
     "Nemotron3ForCausalLM",
     "Nemotron3DenseForCausalLM",
+    # Qwen 3
+    "Qwen3Config",
+    "Qwen3TestConfig",
+    "Qwen3ForCausalLM",
+    "Qwen3MoEForCausalLM",
 ]
