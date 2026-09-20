cf85eb460abec16a9006bd41c3fc67a13769d3b7
sepehratwork <sepehratwork@gmail.com>
2026-09-20 17:33:09 +0330

    Bringing language models into 500 M parameters

--- pipeline/posttrain.py
+++ pipeline/posttrain.py
@@ -82,10 +82,11 @@ def run_stage4_sft(architecture, tokenizer, base_dir, stage3_model_path, hf_user
         print(f"✓ SFT dataset ready with {len(ds):,} conversations.")
 
         args = TrainingArguments(
-            num_train_epochs=2,
+            # num_train_epochs=2,
+            max_steps=6,
             save_total_limit=2, 
             output_dir=stage4_dir, per_device_train_batch_size=1,
-            gradient_accumulation_steps=32, learning_rate=5.0e-5, logging_steps=10, save_steps=500,
+            gradient_accumulation_steps=32, learning_rate=5.0e-5, logging_steps=10, save_steps=50,
             report_to="none", bf16=torch.cuda.is_bf16_supported(), fp16=not torch.cuda.is_bf16_supported(),
             gradient_checkpointing=True,
             gradient_checkpointing_kwargs={"use_reentrant": False},
@@ -208,12 +209,13 @@ def run_stage5_dpo(architecture, tokenizer, base_dir, stage4_model_path, hf_user
         print(f"✓ DPO dataset ready with {len(ds):,} preference pairs.")
 
         args = DPOConfig(
-            num_train_epochs=1,
+            # num_train_epochs=1,
+            max_steps=6,
             save_total_limit=2,
             output_dir=stage5_dir, per_device_train_batch_size=1,
             max_grad_norm=1.0,
             gradient_accumulation_steps=128, learning_rate=8.0e-8, lr_scheduler_type="linear", warmup_ratio=0.1,
-            logging_steps=10, save_steps=500, report_to="none", bf16=torch.cuda.is_bf16_supported(),
+            logging_steps=10, save_steps=50, report_to="none", bf16=torch.cuda.is_bf16_supported(),
             fp16=not torch.cuda.is_bf16_supported(), 
             gradient_checkpointing=True, 
             gradient_checkpointing_kwargs={"use_reentrant": False},
